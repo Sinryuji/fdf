077fa6f8f1a6c50a559a8d2baf8d4dc0dbf4a094
sinryuji <siler2474@gmail.com>
2022-09-11 20:03:02 +0900

    Feat : open windonw

--- include/fdf.h
+++ include/fdf.h
@@ -6,12 +6,14 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 20:24:32 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/09/09 20:24:49 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/09/11 19:47:52 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
+# include "../lib/minilibx_opengl_20191021/mlx.h"
+# include "../lib/libft/include/libft.h"
 
 #endif
--- source/mandatory/fdf.c
+++ source/mandatory/fdf.c
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 20:24:19 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/09/09 20:56:37 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/09/11 19:56:02 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,12 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 
+	void	*mlx;
+	void	*win;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	(void)win;
+    mlx_loop(mlx);
 	return (0);
 }

2efab40b60943f08b590ce1ed08eb98224948975
sinryuji <siler2474@gmail.com>
2022-09-11 21:18:20 +0900

    Feat : pixel set and put

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 20:24:32 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/09/11 19:47:52 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/09/11 20:56:00 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 
 # include "../lib/minilibx_opengl_20191021/mlx.h"
 # include "../lib/libft/include/libft.h"
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
 
 #endif
--- source/mandatory/fdf.c
+++ source/mandatory/fdf.c
@@ -6,11 +6,19 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 20:24:19 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/09/11 19:56:02 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/09/11 21:17:48 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/fdf.h"
+
+void	pixel_set(t_data *data, int x, int y, int color)
+{
+	char	*pixel;
+
+	pixel = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)pixel = color;
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,10 +27,22 @@ int	main(int argc, char **argv)
 
 	void	*mlx;
 	void	*win;
+	t_data	img;
+	int		x;
+	int		y;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 500, 500, "Hello world!");
-	(void)win;
+	img.img = mlx_new_image(mlx, 500, 500);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	x = -1;
+	while (++x < 500)
+	{
+		y = -1;
+		while (++y < 500)
+			pixel_set(&img, x, y, 0x00FF0000);
+	}
+	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
     mlx_loop(mlx);
 	return (0);
 }
